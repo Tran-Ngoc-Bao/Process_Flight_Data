5930cb7e6c0875f30729f8845c287188cc316715
Tran-Ngoc-Bao <tranngocbaooooo12062003@gmail.com>
2024-11-25 17:50:13 +0700

    Demo Transform Data

---
+++ spark/transform_data.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+
+def solution():
+    global year
+    global month
+
+    df = spark.read.parquet("hdfs://namenode:9000/staging/" + str(year) + "/" + str(month))
+    df.repartition(1).write.mode("overwrite").parquet()
+
+    if month == 12:
+        year += 1
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("Insert Data Warehouse").getOrCreate()
+
+    year = 2018
+    month = 1
+
+    flag = True
+    while flag:
+        try:
+            solution()
+        except:
+            print("Don't worry about this error", year, month)
+            flag = False
+    # solution()
+
+    # Select columns not too much null
+    # Process duplicated rows
+    # Process date data
+    # Cast data type
+    # Insert data
